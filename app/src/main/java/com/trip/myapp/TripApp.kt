e43b172f66fd32f7a6ea6e3821ee3ff6a9cad623
easyhak <jooin2000@cau.ac.kr>
2024-11-07 01:15:17 +0900

    폴더 구조 세팅

---
+++ app/src/main/java/com/trip/myapp/TripApp.kt
@@ -0,0 +1,8 @@
+package com.trip.myapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TripApp : Application() {
+}
---
+++ app/src/main/java/com/trip/myapp/data/model/Sample.kt
@@ -0,0 +1,6 @@
+package com.trip.myapp.data.model
+
+data class Sample (
+    val name: String,
+    val age: Int
+)
