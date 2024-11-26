f91e164383faf77d5f5441036ab5267e569f92c6
easyhak <jooin2000@naver.com>
2024-11-26 14:04:40 +0900

    chore

--- app/src/main/java/com/trip/myapp/ui/archive/ArchiveViewModel.kt
+++ app/src/main/java/com/trip/myapp/ui/archive/ArchiveViewModel.kt
@@ -3,19 +3,25 @@ package com.trip.myapp.ui.archive
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.trip.myapp.R
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 
-class ArchiveViewModel : ViewModel() {
+@HiltViewModel
+class ArchiveViewModel @Inject constructor(
+
+) : ViewModel() {
 
     private val _folders = MutableStateFlow(
         listOf(
-            Folder(name = "전체", imageRes = R.drawable.category_sample_2, isDefault = true)
+            Folder(name = "전체", imageRes = R.drawable.category_sample_2)
         )
     )
-    val folders: StateFlow<List<Folder>> get() = _folders
+    val folders: StateFlow<List<Folder>> = _folders.asStateFlow()
 
     fun addFolder(folderName: String) {
         viewModelScope.launch {
